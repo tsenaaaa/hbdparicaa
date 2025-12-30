fdfa04db11e5b00a135f2f5709630a20cc8eb7cf
tsenaaaa <abimbimasena36@gmail.com>
2025-12-30 07:27:01 +0700

    first scrapbook website

---
+++ script.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      sec.style.opacity = 1;
+      sec.style.transform = "translateY(0)";
+    }
+  });
+});
+
+sections.forEach(sec => {
+  sec.style.opacity = 0;
+  sec.style.transform = "translateY(50px)";
+  sec.style.transition = "all 1s ease";
+});
